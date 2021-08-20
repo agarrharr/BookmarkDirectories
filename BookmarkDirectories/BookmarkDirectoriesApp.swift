d766ec420fdeaca2d3bfea00a6a0e35de6887f9f
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-08-20 11:20:42 -0600

    Initial Commit

---
+++ BookmarkDirectories/BookmarkDirectoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkDirectoriesApp.swift
+//  BookmarkDirectories
+//
+//  Created by Adam Garrett-Harris on 8/20/21.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkDirectoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookmarkDirectories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookmarkDirectories
+//
+//  Created by Adam Garrett-Harris on 8/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
